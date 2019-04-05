c5be5b8c665bcdc2411c07e8fa9b9abe79292778
charliemedina <charliemedina1989@gmail.com>
2019-04-05 21:39:48 +0200

    Clean node modules folder

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+        <p>
+          This app uses React, Redux, React Router, and many other helpful
+          libraries.
+        </p>
+      </div>
+    );
+  }
+}
+export default AboutPage;
