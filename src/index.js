daaea63f878d3df6f66cf6280b652c2599561330
charliemedina <charliemedina1989@gmail.com>
2019-04-15 13:15:12 +0200

    - Instantiate Store and Provider - Connect Container - Step Through Redux Flow

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import 'babel-polyfill';
-import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import {render} from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+import {Router, browserHistory} from 'react-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import routes from './routes';
 import './styles/styles.css';
 
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById("app")
 );
